787e1ac6e6ab809dab800b0659dadf326cf8dadf
mohankalahasthi <mohanpradeep2@gmail.com>
2019-05-13 22:58:40 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderNavComponent
+    HeaderNavComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
